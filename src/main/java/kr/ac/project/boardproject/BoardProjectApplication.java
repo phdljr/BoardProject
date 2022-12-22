c4e82822937e4b141f660c25d15012a6963b4669
phdljr <phdljr@naver.com>
2022-12-22 21:39:55 +0900

    first commit

---
+++ src/main/java/kr/ac/project/boardproject/BoardProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.project.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/project/boardproject/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.project.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
