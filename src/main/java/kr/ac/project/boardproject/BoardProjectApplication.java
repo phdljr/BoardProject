915cb3e8621f485e6c8ac92bcb5de2f62b804e19
phdljr <phdljr@naver.com>
2022-12-22 22:53:22 +0900

    init: JPA 설정

--- src/main/java/kr/ac/project/boardproject/BoardProjectApplication.java
+++ src/main/java/kr/ac/project/boardproject/BoardProjectApplication.java
@@ -1,9 +1,12 @@
 package kr.ac.project.boardproject;
 
+import kr.ac.project.boardproject.entity.Board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardProjectApplication {
 
     public static void main(String[] args) {
