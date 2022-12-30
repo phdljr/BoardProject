95c39d841e7aae96605d648e9450f55182943a8a
phdljr <phdljr@naver.com>
2022-12-30 19:07:09 +0900

    test: 더미 데이터 테스트 코드 추가

---
+++ src/test/java/kr/ac/project/boardproject/setup/SetUpDummyData.java
@@ -0,0 +1,83 @@
+package kr.ac.project.boardproject.setup;
+
+import kr.ac.project.boardproject.entity.Board;
+import kr.ac.project.boardproject.entity.Member;
+import kr.ac.project.boardproject.entity.MemberType;
+import kr.ac.project.boardproject.entity.Reply;
+import kr.ac.project.boardproject.repository.BoardRepository;
+import kr.ac.project.boardproject.repository.MemberRepository;
+import kr.ac.project.boardproject.repository.ReplyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.LongStream;
+
+@SpringBootTest
+public class SetUpDummyData {
+
+    @Autowired
+    protected MemberRepository memberRepository;
+    @Autowired
+    protected BoardRepository boardRepository;
+    @Autowired
+    protected ReplyRepository replyRepository;
+
+    protected Member member;
+    protected Board board;
+    protected Reply reply;
+
+    @Test
+    public void createDummyData() {
+        member = Member.builder()
+                .email("test@test.test")
+                .memberType(MemberType.USER)
+                .nickname("testNickname")
+                .password("testPassword")
+                .build();
+        member = memberRepository.save(member);
+        board = Board.builder()
+                .member(member)
+                .title("testTitle")
+                .content("testContent")
+                .build();
+        board = boardRepository.save(board);
+        reply = Reply.builder()
+                .board(board)
+                .member(member)
+                .content("testReplyContent")
+                .build();
+        reply = replyRepository.save(reply);
+    }
+
+    @Test
+    public void createDummyDataList() {
+        LongStream.range(1, 16).forEach(i -> {
+            Member member = Member.builder()
+                    .email("test@test.test" + i)
+                    .memberType(MemberType.USER)
+                    .nickname("test" + i)
+                    .password("tttt" + i)
+                    .build();
+            member = memberRepository.save(member);
+
+            Board board = Board.builder()
+                    .member(member)
+                    .title("test" + i)
+                    .content("" + i)
+                    .build();
+            board = boardRepository.save(board);
+
+            Board finalBoard = board;
+            Member finalMember = member;
+            LongStream.range(1, 6).forEach(j -> {
+                Reply reply = Reply.builder()
+                        .board(finalBoard)
+                        .member(finalMember)
+                        .content("test")
+                        .build();
+                replyRepository.save(reply);
+            });
+        });
+    }
+}
